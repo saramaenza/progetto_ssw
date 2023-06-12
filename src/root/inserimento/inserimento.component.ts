507565f5f775bc2f6fce6657c61cd54f75aa73da
saramaenza <61665335+saramaenza@users.noreply.github.com>
2023-06-12 20:24:50 +0200

    3 files modified, 4 added

---
+++ src/root/inserimento/inserimento.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-inserimento',
+  templateUrl: './inserimento.component.html',
+  styleUrls: ['./inserimento.component.css'],
+  imports: [CommonModule],
+  standalone: true,
+})
+export class InserimentoComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ src/root/ricerca/ricerca.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ricerca',
+  templateUrl: './ricerca.component.html',
+  styleUrls: ['./ricerca.component.css'],
+  imports: [CommonModule],
+  standalone: true,
+})
+export class RicercaComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
